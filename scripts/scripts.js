c6db6e4064125343d862a7c50c0080876b73f2b3
Cadell Julien <cjulien2019@fau.edu>
2023-02-06 19:52:52 -0500

    Added a 2d array for board placement

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,3 +16,14 @@ let computerScore=0; //for cpu score
 
 const xPiece = 'x'; //X shower
 const OPiece = 'o'; //O SHower
+
+const validWins = [
+	[ 0, 1, 2 ],
+	[ 3, 4, 5 ],
+	[ 6, 7, 8 ],
+	[ 0, 3, 6 ],
+	[ 1, 4, 7 ],
+	[ 2, 5, 8 ],
+	[ 0, 4, 8 ],
+	[ 2, 4, 6 ]
+]; //Winning Placements and also a 2d array
